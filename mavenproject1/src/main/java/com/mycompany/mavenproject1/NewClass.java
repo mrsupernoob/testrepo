3eef035c6a2804ebbcc6ed7d40ec92c81f459642
Evgeny <Evgeny@OldComputer>
2016-03-29 23:13:39 +0400

    1

---
+++ mavenproject1/src/main/java/com/mycompany/mavenproject1/NewClass.java
@@ -0,0 +1,15 @@
+package com.mycompany.mavenproject1;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Evgeny
+ */
+public class NewClass {
+    
+}
